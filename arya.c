d8f624a7fd1e50a903cb5b341fcd4aaa39d43368
aryakk4 <aryakk4@gmail.com>
2017-02-01 10:01:32 +0530

    files are added

---
+++ arya.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+        int a, b;
+
+	printf("Enter the numbers\n");
+	scanf("%d%d", &a, &b);
+	printf("Sum= %d\n", a+b);
+	printf("program successed");
+	return 0;
+}
